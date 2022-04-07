0eb0b404419cefffdca5bc250ddbec9287821543
unknown <venu2455@gmail.com>
2022-04-07 23:06:59 +0530

    adding api

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,45 +6,71 @@ const lodash = require('lodash')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    loggerModule.welcomeMessage()
-    helperModule.printTodaysDate()
-    helperModule.printCurrentMonth()
-    helperModule.printBatchInformation()
-    formatterModule.trimString()
-    formatterModule.changeCaseToUpper()
-    formatterModule.changeCaseToLower()
-    res.send('My first ever api!')
+// router.get('/test-me', function (req, res) {
+//     loggerModule.welcomeMessage()
+//     helperModule.printTodaysDate()
+//     helperModule.printCurrentMonth()
+//     helperModule.printBatchInformation()
+//     formatterModule.trimString()
+//     formatterModule.changeCaseToUpper()
+//     formatterModule.changeCaseToLower()
+//     res.send('My first ever api!')
+// });
+
+router.get('/movies', function (req, res) {
+let movies=["king","puspha","RRR","Ustadhotel"]
+console.log('the list of movies '+" "+movies)
+
+res.send('the list of movies'+" "+movies)
 });
+router.get('/movies/:indexNumber', function (req, res) {
+    let movies=["king","puspha","RRR","Ustadhotel"]
+    console.log('the list of movies ',req.params.indexNumber)
+    
+    res.send(movies[req.params.indexNumber])
+    });
+router.get('/movies/:indexNumber', function (req, res) {
+    let arrMovies = ["king", "puspha",  "RRR", "ustadhotel"];
 
-router.get('/hello', function (req, res) {
-// Problem a)
-let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-let subArrays = lodash.chunk(months, 3)
-console.log('The result after splitting the months array is ', subArrays)
-
-// Problem b)
-
-let oddNumbers = [1,3,5,7,9,11,13,15,17,19]
-console.log('The last 9 odd numbers in the array are: ', lodash.tail(oddNumbers))
-
-// Problem c)
-let a = [1 , 2, 1, 4]
-let b = [2, 3, 4, 3]
-let c = [6, 1, 5, 10]
-let d = [1, 1, 1]
-let e = [1, 2, 3, 4, 5]
-
-console.log('Final array or unique numbers is : ', lodash.union(a, b, c, d, e))
-
-// Problem d)
-let arrayOfKeyValuePairs = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
-console.log('The object created from arrays is :', lodash.fromPairs(arrayOfKeyValuePairs))
-    res.send('My hello api!')
+    if (req.params.indexNumber <= arrMovies.length)
+        res.send(arrMovies[req.params.indexNumber]);
+    else res.send('use a valid index number');
 });
+router.get('/films', function (req, res) {
+    let arrMovies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }];
 
+    res.send(arrMovies);
+});
+router.get('/films/:indexnumber', function (req, res) {
+    let arrMovies = [{
+        "id": 1,
+        "name": "The Shining"
+    }, {
+        "id": 2,
+        "name": "Incendies"
+    }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+    }, {
+        "id": 4,
+        "name": "Finding Nemo"
+    }];
 
-
-
+    if (req.params.indexnumber <= arrMovies.length)
+            res.send(arrMovies[req.params.indexnumber]);
+       else res.send('No movie exists with this id')   
+});
 module.exports = router;
 // adding this comment for no reason
